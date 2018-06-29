3ab60a2176f1de0697d14c0e01c48e799c7cdf8e
peng <pengguiyang1@126.com>
2018-06-29 22:47:51 +0800

    2018-06-29 22:46:55

---
+++ src/main/java/vip/oldboy/OldBoy.java
@@ -0,0 +1,13 @@
+package vip.oldboy;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by peng on 18/6/29.
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface OldBoy {
+
+}
